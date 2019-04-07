b58558f91aa0cefa3514cbd753dc22320ea1a050
Luiz Galiza <lfdgaliza@gmail.com>
2019-04-07 14:55:56 +0100

    server side ok

---
+++ WebApplication/Constants/AuthConstant.cs
@@ -0,0 +1,8 @@
+namespace WebApplication.Constants
+{
+    // It must be in a more secure place.
+    public class AuthConstant
+    {
+        public const string Key = "a-very-big-key-to-pass";
+    }
+}
