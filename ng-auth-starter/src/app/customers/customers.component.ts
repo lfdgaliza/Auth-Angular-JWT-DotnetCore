5b0818f6efc785b517319ca3892f5cd3638b042d
Luiz Galiza <lfdgaliza@gmail.com>
2019-04-13 16:23:24 +0100

    login working on client side

--- ng-auth-starter/src/app/customers/customers.component.ts
+++ ng-auth-starter/src/app/customers/customers.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
-  styleUrls: []
+  styles: [``]
 })
-export class CustomersComponent  {
+export class CustomersComponent implements OnInit {
+  customers: any;
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    let token = localStorage.getItem("jwt");
+    this.http.get("https://localhost:5001/api/customers", {
+      headers: new HttpHeaders({
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/json"
+      })
+    }).subscribe(response => {
+      this.customers = response;
+    }, err => {
+      console.log(err)
+    });
   }
+}
